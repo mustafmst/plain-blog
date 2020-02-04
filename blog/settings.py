4323b94502b898255933ed1eb553ffef8e221721
Paweł Mstowski <pawel.mstowskio@gmail.com>
2020-02-04 20:12:53 +0100

    Add .env file handling for local development

--- blog/settings.py
+++ blog/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from os import environ
 
+
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
