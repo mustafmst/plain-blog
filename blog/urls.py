bc1e06cd573a80e1bb0025560f0b93cc1e0084bc
Pawel Mstowski <pawel.mstowski@gmail.com>
2020-02-10 11:29:13 +0100

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+
+admin.site.site_header = 'Simple-Blog Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/blog/', include('blogging_api.urls')),
